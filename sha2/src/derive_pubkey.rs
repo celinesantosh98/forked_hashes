3c8d67f668cf9d6ca62e60710a78c3ef7fca1645
c.santosh <c.santosh@gmail.com>
2025-08-20 09:12:29 +0200

    emitted 2 events success

--- sha2/src/derive_pubkey.rs
+++ sha2/src/derive_pubkey.rs
@@ -9,6 +9,7 @@ use p256::{EncodedPoint, SecretKey};
 use p256::{elliptic_curve::sec1::ToEncodedPoint};
 
 use crate::bindings::mwrapper::crypto::monitorkey;
+use base64::{engine::general_purpose, Engine as _};
 
 #[cfg(feature = "use-curve25519")]
 pub struct Monitored_Dh25519 {
@@ -39,7 +40,7 @@ impl Monitored_Dh25519 {
         self.pubkey = point.to_bytes();
 
          // Call monitor
-        monitorkey::monitor_ephemeral(&self.pubkey);
+        //monitorkey::mon_exp_g_from_priv(&self.privkey);
     }
 }
 
@@ -52,4 +53,46 @@ impl Monitored_P256 {
         let encoded_pub = public_key.to_encoded_point(false);
         self.pubkey = encoded_pub;
     }
+}
+
+// Emit after RNG-based key generation (rand + exp(g,ltk))
+pub fn emit_after_generate(privkey: &[u8]) {
+    monitorkey::mon_rand();
+    monitorkey::mon_exp_g_from_priv(privkey);
+}
+
+// Emit after DH (exp(peer_pk, my_priv))
+pub fn emit_after_dh(peer_pk: &[u8], my_priv: &[u8]) {
+    monitorkey::mon_dh_shared(peer_pk, my_priv);
+}
+
+pub fn emit_setup_static(ltk: &[u8]) {
+    monitorkey::mon_setup_static(ltk);
+}
+
+// pub fn emit_init_peer(ltk_i: [u8;32], pk_r: [u8;32]) {
+//     let args = base64::engine::general_purpose::STANDARD.encode(ltk_i) + "," +
+//            &base64::engine::general_purpose::STANDARD.encode(pk_r);
+//     // let args = format!("{},{}",
+//     //     base64::engine::general_purpose::STANDARD.encode(ltk_i),
+//     //     base64::engine::general_purpose::STANDARD.encode(pk_r));
+//          monitorkey::mon_init_peer(&ltk_i[..], &pk_r[..]);
+//     //eprintln!("[mon] init({},...) -> {}", args, ok);
+// }
+
+// pub fn emit_init_peer(ltk: &[u8], pk_r: &[u8]) {
+//     monitorkey::mon_init_peer(ltk, pk_r);
+// }
+
+//  if ok != 1 { eprintln!("monitor rejected init"); }
+//}
+
+// pub fn emit_init_peer(ltk_i: &[u8; 32], pk_r: &[u8]) {
+//     // pass raw bytes; the wrapper will serialize/base64 + add comma
+//     monitorkey::mon_init_peer(&ltk_i[..], pk_r);
+// }
+
+pub fn emit_init_peer(ltk_i: &[u8], pk_r: &[u8]) {
+    // pass raw bytes; wrapper does the base64 + formatting
+    monitorkey::mon_init_peer(ltk_i, pk_r);
 }
