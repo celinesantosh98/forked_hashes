48177dd57bec9922d801bc4a9a285b56eb6038f9
c.santosh <c.santosh@gmail.com>
2025-09-16 22:01:18 +0200

    setup,init,precompute, hash_pair, rand accepted

--- sha2/src/derive_pubkey.rs
+++ sha2/src/derive_pubkey.rs
@@ -10,6 +10,10 @@ use p256::{elliptic_curve::sec1::ToEncodedPoint};
 
 use crate::bindings::mwrapper::crypto::monitorkey;
 use base64::{engine::general_purpose, Engine as _};
+#[cfg(not(feature = "std"))]
+extern crate alloc;
+#[cfg(not(feature = "std"))]
+use alloc::vec::Vec;
 
 #[cfg(feature = "use-curve25519")]
 pub struct Monitored_Dh25519 {
@@ -56,9 +60,18 @@ impl Monitored_P256 {
 }
 
 // Emit after RNG-based key generation (rand + exp(g,ltk))
+// pub fn emit_after_generate(privkey: &[u8]) {
+//    // monitorkey::mon_rand();
+//     monitorkey::mon_exp_g_from_priv(privkey);
+// }
 pub fn emit_after_generate(privkey: &[u8]) {
-    monitorkey::mon_rand();
-    monitorkey::mon_exp_g_from_priv(privkey);
+    // DO NOT: monitorkey::mon_rand();
+
+    // compute public = exp(g,priv) the same way Snow does:
+    let pubkey = MontgomeryPoint::mul_base_clamped(*arrayref::array_ref![privkey, 0, 32]).to_bytes();
+
+    // pair(exp('g', ltkI), expgltkI)
+    monitorkey::mon_exp_g_pair(privkey, &pubkey);
 }
 
 // Emit after DH (exp(peer_pk, my_priv))
@@ -70,29 +83,42 @@ pub fn emit_setup_static(ltk: &[u8]) {
     monitorkey::mon_setup_static(ltk);
 }
 
-// pub fn emit_init_peer(ltk_i: [u8;32], pk_r: [u8;32]) {
-//     let args = base64::engine::general_purpose::STANDARD.encode(ltk_i) + "," +
-//            &base64::engine::general_purpose::STANDARD.encode(pk_r);
-//     // let args = format!("{},{}",
-//     //     base64::engine::general_purpose::STANDARD.encode(ltk_i),
-//     //     base64::engine::general_purpose::STANDARD.encode(pk_r));
-//          monitorkey::mon_init_peer(&ltk_i[..], &pk_r[..]);
-//     //eprintln!("[mon] init({},...) -> {}", args, ok);
-// }
-
-// pub fn emit_init_peer(ltk: &[u8], pk_r: &[u8]) {
-//     monitorkey::mon_init_peer(ltk, pk_r);
-// }
-
-//  if ok != 1 { eprintln!("monitor rejected init"); }
-//}
-
-// pub fn emit_init_peer(ltk_i: &[u8; 32], pk_r: &[u8]) {
-//     // pass raw bytes; the wrapper will serialize/base64 + add comma
-//     monitorkey::mon_init_peer(&ltk_i[..], pk_r);
-// }
-
 pub fn emit_init_peer(ltk_i: &[u8], pk_r: &[u8]) {
-    // pass raw bytes; wrapper does the base64 + formatting
     monitorkey::mon_init_peer(ltk_i, pk_r);
 }
+
+// pub fn emit_receive(channel: &str, msg: &[u8]) {
+//     monitorkey::mon_receive(channel, msg);
+// }
+
+// pub fn emit_send(channel: &str, msg: &[u8]) {
+//     monitorkey::mon_send(channel, msg);
+// }
+
+// pub fn emit_hash(proto: &str) {
+//     monitorkey::mon_hash(proto);
+// }
+// pub fn emit_hash_pair(proto: &str, digest32: &[u8]) {
+//     monitorkey::mon_hash_pair(proto, digest32);
+// }
+pub fn emit_hash_pair(proto: &str, _digest32: &[u8]) {
+    // The monitor rules use a symbolic atom like: hNoise_<proto>
+    let mut tag: Vec<u8> = Vec::with_capacity(1 + proto.len());
+    tag.push(b'h');
+    tag.extend_from_slice(proto.as_bytes());
+
+    // Pass a byte slice (Vec<u8> -> &[u8])
+    monitorkey::mon_hash_pair(proto, &tag);
+}
+
+pub fn emit_precompute(ltk: &[u8]) {
+    monitorkey::mon_precompute(ltk);
+}
+pub fn emit_ephemeral_rand(ephemeral_priv: &[u8]) {
+   monitorkey::mon_rand(ephemeral_priv);
+}
+// pub fn emit_ephemeral_rand(ephemeral_priv: &[u8]) {
+//     // Send ⟨rand(), n⟩ with n = ephemeral_priv (raw bytes).
+//     // Do NOT base64 here; the return must be a ground byte-string.
+//     monitorkey::mon_rand(ephemeral_priv);
+// }
